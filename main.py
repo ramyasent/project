7850274b96896d7c82d31de0f394019c52e6782b
admin123 <sangeetha.v@egrovesys.com>
2023-07-20 04:13:43 -0400

    created main.py
    some description

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import requests
+lc_base_url = 'https://linkcheckerapi.elitesiteoptimizer.com/'
+LC_SECURITY_KEY = "GhfmGu8IY5ndzG8GjC3euKrOv"
+org_slug='reapp'
+domain_slug='elitemcommerce'
+request_url = os.path.join(lc_base_url, "summary_data", org_slug, domain_slug)
+
+if start_date and end_date:
+    request_params = {"key": LC_SECURITY_KEY,
+                      "start_date": start_date,
+                      "end_date": end_date,
+                      "is_dashboard": True,
+                      "status_code": True,
+                      "section_name": section_name,
+                      }
+else:
+    request_params = {"key": settings.LC_SECURITY_KEY,
+                      "section_name": section_name,
+                      "status_code": True
+                      }
+try:
+    res = requests.get(request_url, params=request_params)
+    status_code = res.status_code
+except:
+    status_code = None
+
+if status_code == 200:
+    res_json = res.json()
+    print('res_json', res_json)
